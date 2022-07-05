1f5a6f3313e1252f6e28c4c442f6b4d0e10130a9
AlecefeMartinez <104871029+AlecefeMartinez@users.noreply.github.com>
2022-07-05 18:15:16 -0300

    Update musicianBodyValidator.js

--- src/validations/musicianBodyValidator.js
+++ src/validations/musicianBodyValidator.js
@@ -1,30 +1,12 @@
-const Joi = require("joi")
+const Joi = require('joi')
 
-const bodySchema = Joi.object({
-  firstName: Joi.string().alphanum().min(3).max(30).trim().required(),
-  lastName: Joi.string().alphanum().min(3).max(30).trim().required(),
-  instrument: Joi.string().required(),
-  orchestra: Joi.string().required()
-})
-
-const paramsSchema = Joi.object({
-  id: Joi.string().alphanum().max(24).required()
-})
-
-const querySchema = Joi.alternatives().try(
-  Joi.object({
-    firstName: Joi.string().required()
-  }),
-  Joi.object({
-    lastName: Joi.string().required()
-  }),
-  Joi.object({
-    instrument: Joi.string().required()
-  }),
-  Joi.object({
+const schema = Joi.object(
+  {
+    firstName: Joi.string().alphanum().min(3).max(30).trim().required(),
+    lastName: Joi.string().alphanum().min(3).max(30).trim().required(),
+    instrument: Joi.string().required(),
     orchestra: Joi.string().required()
-  }),
-  Joi.object({})
+  }
 )
 
-module.exports = { bodySchema, paramsSchema, querySchema }
+module.exports = schema
